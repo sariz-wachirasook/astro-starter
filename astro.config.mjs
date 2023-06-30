ec356720bc68f5e1b477af3b2096d62b87885d19
Sariz Outsourcify <sariz@outsourcify.net>
2023-06-30 15:34:00 +0700

    feat: update SEO, TailwindCSS, SASS, XML Sitemap, Robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-starter.com',
+
+  integrations: [
+    // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
+    sitemap(),
+
+    // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+    robotsTxt(),
+
+    // @astrojs/tailwind: https://docs.astro.build/en/guides/integrations-guide/tailwind/
+    tailwind(),
+
+    // @astrojs/image: https://docs.astro.build/en/guides/integrations-guide/image/
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
+});
