4f67a68d90b8043f118eb68849b5ba7d864bfe7f
sariz_wachirasook <sariz.wachirasook@gmail.com>
2023-07-03 01:18:29 +0700

    feat: add partytown for third-party scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,26 @@ import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import astroI18next from 'astro-i18next';
 import compress from 'astro-compress';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-starter.com',
   integrations: [
-    // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
-    sitemap(),
-    // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
-    robotsTxt(),
-    // @astrojs/tailwind: https://docs.astro.build/en/guides/integrations-guide/tailwind/
-    tailwind(),
-    // @astrojs/image: https://docs.astro.build/en/guides/integrations-guide/image/
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    // astro-i18next: https://github.com/yassinedoghri/astro-i18next#readme
-    astroI18next(),
-    // astro-compress: https://github.com/astro-community/astro-compress#readme
-    compress(),
-  ],
+  // @astrojs/sitemap: https://docs.astro.build/en/guides/integrations-guide/sitemap/
+  sitemap(),
+  // astro-robots-txt: https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+  robotsTxt(),
+  // @astrojs/tailwind: https://docs.astro.build/en/guides/integrations-guide/tailwind/
+  tailwind(),
+  // @astrojs/image: https://docs.astro.build/en/guides/integrations-guide/image/
+  image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }),
+  // astro-i18next: https://github.com/yassinedoghri/astro-i18next#readme
+  astroI18next(),
+  // astro-compress: https://github.com/astro-community/astro-compress#readme
+  compress(), 
+  // @astrojs/partytown: https://docs.astro.build/en/guides/integrations-guide/partytown/
+  partytown()]
 });
